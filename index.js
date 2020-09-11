b0851d5356869d017a041d51216abc77820a8578
Snugglywums <69602411+Snugglywums@users.noreply.github.com>
2020-09-11 14:19:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ function displayResults(responseJson) {
   for (let i = 0; i < responseJson.data.length; i++){
     // for each park object in the items array, add a list item to the results 
     //list with the park name, description, and url
+    let index = 0;
     $('#results-list').append(
       `<li><h3>${responseJson.data[i].fullName}</h3>
+      <h4>${responseJson.data[i].addresses[index].line1}, ${responseJson.data[i].addresses[index].city}, ${responseJson.data[i].addresses[index].stateCode}</h4>
       <p>${responseJson.data[i].description}</p>
       <p>${responseJson.data[i].url}</p>
       </li>`
@@ -30,35 +32,46 @@ function displayResults(responseJson) {
 };
 
 function getParksList(limit=50) {
- const stateSearch = $('#js-search-term').val();
- console.log(stateSearch);
-  const params = {
-    limit,
-  };
-  const queryString = formatQueryParams(params)
-  const url = searchURL + stateSearch + '&api_key='+ apiKey;
+  const multipleStateSearch = $('#js-search-multiple').val();
 
-  console.log(url);
+ //console.log(multipleStateSearch);
+
+let stateSearch = multipleStateSearch.split(',');
+//console.log(stateSearch);
+
+for( let i = 0; i < stateSearch.length; i++){
+  const url = searchURL + stateSearch[i] + '&api_key='+ apiKey;
+ 
 
   fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then(responseJson => displayResults(responseJson))
+  .catch(err => {
+    $('#js-error-message').text(`Something went wrong: ${err.message}`);
+  });
 }
+
+
+  const params = {
+   limit,
+  };
+
+  const queryString = formatQueryParams(params)
+
+}
+
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     const limit = $('#js-max-results').val();
-    const stateSearch = $('#js-search-term').val();
-    getParksList(stateSearch, limit);
+    const multipleStateSearch = $('#js-search-multiple').val();
+    getParksList(multipleStateSearch, limit);
   });
 }
 
